13844f65ff7cb084ccf47c84cdbcafdf49c30c25
Turtles-are-good <26383@student.macleans.school.nz>
2026-06-10 04:26:05 +0000

    My work

--- Sets-work.py
+++ Sets-work.py
@@ -1,1 +1,12 @@
-
+word = input("Enter a word or number \n")
+a = set(word)
+limit = int(input("How much letters do you want to remove? \n"))
+count = 0
+if limit > 0 :
+    while limit > count :
+        phrase = input("Enter a letter to remove \n")
+        a.discard(phrase)
+        count += 1
+else :
+    print("Enter a correct limit")
+print(f"Your set is: \n {a}")
